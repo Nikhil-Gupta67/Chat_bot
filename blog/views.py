ebfb47db5e7af3e5b5b93b1cdc1c5763d5c3909f
Nikhil-Gupta76 <nikhilgupta8595665@gmail.com>
2026-03-03 18:22:39 +0530

    Modified the urls

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the blog index.")
-
 def specific(request):
     return HttpResponse("This is a specific view in the blog app.")
 
