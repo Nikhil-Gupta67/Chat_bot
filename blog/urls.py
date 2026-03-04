97c4d363c84eceee6446655c5b9d66038a98a52e
Nikhil-Gupta76 <nikhilgupta8595665@gmail.com>
2026-03-04 20:34:53 +0530

    Modified the urls

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('get_response/',views.get_response,name='get_response'),
 
 
-
     # path('article/<int:article_id>/', views.article_detail, name='article_detail'),  ##(article/<int:article_id>/) this allow to pass the number in the urls
 ]
